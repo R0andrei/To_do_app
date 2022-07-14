f61982a463dcbe275f78b2d165d9b55f46710bed
R0andrei <108833337+R0andrei@users.noreply.github.com>
2022-07-14 23:47:54 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 var index=1;
+var myCheck=[];
 
 function Add(){
-    let lista = document.getElementsByTagName("li");
+    let list = document.getElementsByTagName("li");
     let task = document.getElementById("text").value;
     let activity = document.getElementsByClassName("activity");
-    if(index>lista.length){
-        alert("You've reached maximum number of tasks");
+    if(index>list.length){
+        alert("You've reached the maximum number of tasks");
     }
     activity[index-1].style.display="block";
-    lista[index-1].innerText=task;
+    list[index-1].innerText=task;
+    myCheck[index-1]="new";
     index++;
     }
 function Ready(n){
     let done = document.getElementsByTagName("li");
     done[n].style.color="greenyellow";
     done[n].style.textDecoration="line-through";
+    myCheck[n]="rdy";
 }
 function Delete(n){
     let close = document.getElementsByTagName("li");
     let activity = document.getElementsByClassName("activity");
-    if(n==9){
+    let len = close.length;
+    if(n==len){
         activity[n].style.display="none";
         index--;
-        close[n].style.color="white";
-        close[n].style.textDecoration="none";
+        myCheck[n]="new";
     }
     else{
         for(let i=n; i<index-2; i++){
             close[i].innerText=close[i+1].innerText;
-        }
-        for(let j=0; j<10; j++)
-        {close[j].style.color="white";
-        close[j].style.textDecoration="none";}
+            myCheck[i]=myCheck[i+1];
+    }
         activity[index-2].style.display="none";
+        myCheck[index-2]="new";
         index--;
+    }
+    mystyle();
+}
+
+function mystyle(){
+    let list = document.getElementsByTagName("li");
+    let len = list.length;
+    for(i=0;i<len;i++){
+        if(myCheck[i]==="rdy"){
+            list[i].style.color="greenyellow";
+            list[i].style.textDecoration="line-through";
+        }
+        else{
+            list[i].style.color="white";
+            list[i].style.textDecoration="none";
+        }
     }
 }
