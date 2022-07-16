c37cf86b5d909adb083818803fd1a48dfdc8f415
R0andrei <108833337+R0andrei@users.noreply.github.com>
2022-07-16 21:49:36 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
-var index=1;
-var myCheck=[];
+var index = 1;
+var myCheck = [];
+var list = document.getElementsByTagName("li");
+var activity = document.getElementsByClassName("activity");
+var len = list.length;
 
-function Add(){
-    let list = document.getElementsByTagName("li");
-    let task = document.getElementById("text").value;
-    let activity = document.getElementsByClassName("activity");
-    if(index>list.length){
+function Add() {
+    let task = document.getElementById("text").value; //doar ID
+    if (index > list.length) {
         alert("You've reached the maximum number of tasks");
     }
-    activity[index-1].style.display="block";
-    list[index-1].innerText=task;
-    myCheck[index-1]="new";
+    activity[index - 1].style.display = "block";
+    list[index - 1].innerText = task;
+    myCheck[index - 1] = "new";
     index++;
-    }
-function Ready(n){
-    let done = document.getElementsByTagName("li");
-    done[n].style.color="greenyellow";
-    done[n].style.textDecoration="line-through";
-    myCheck[n]="rdy";
 }
-function Delete(n){
-    let close = document.getElementsByTagName("li");
-    let activity = document.getElementsByClassName("activity");
-    let len = close.length;
-    if(n==len){
-        activity[n].style.display="none";
+function Ready(n) {
+    list[n].style.color = "greenyellow";
+    list[n].style.textDecoration = "line-through";
+    myCheck[n] = "rdy";
+}
+function Delete(n) {
+    if (n == len) {
+        activity[n].style.display = "none";
         index--;
-        myCheck[n]="new";
+        myCheck[n] = "new";
     }
-    else{
-        for(let i=n; i<index-2; i++){
-            close[i].innerText=close[i+1].innerText;
-            myCheck[i]=myCheck[i+1];
-    }
-        activity[index-2].style.display="none";
-        myCheck[index-2]="new";
+    else {
+        for (let i = n; i < index - 2; i++) {
+            list[i].innerText = list[i + 1].innerText;
+            myCheck[i] = myCheck[i + 1];
+        }
+        activity[index - 2].style.display = "none";
+        myCheck[index - 2] = "new";
         index--;
     }
     myStyle();
 }
 
-function myStyle(){
-    let list = document.getElementsByTagName("li");
-    let len = list.length;
-    for(i=0;i<len;i++){
-        if(myCheck[i]==="rdy"){
-            list[i].style.color="greenyellow";
-            list[i].style.textDecoration="line-through";
+function myStyle() {
+    for (i = 0; i < len; i++) {
+        if (myCheck[i] === "rdy") {
+            list[i].style.color = "greenyellow";
+            list[i].style.textDecoration = "line-through";
         }
-        else{
-            list[i].style.color="white";
-            list[i].style.textDecoration="none";
+        else {
+            list[i].style.color = "white";
+            list[i].style.textDecoration = "none";
         }
     }
 }
